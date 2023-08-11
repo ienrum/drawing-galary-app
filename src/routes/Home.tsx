e102f9630e50c7e5eaf7b85dd5c1f4fa0c4d59b3
ienrum <azang0506@gmail.com>
2023-08-11 19:13:19 +0900

    feat: title 과 galary 적용한 home component 추가

---
+++ src/routes/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Title from "../components/UI/Title";
+import Galary from "../components/Galary";
+
+const Home = () => {
+  return (
+    <>
+      <Title title="Home" subTitle="Welcome to Home Page" />
+      <Galary />
+    </>
+  );
+};
+export default Home;
